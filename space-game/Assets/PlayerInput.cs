b6557e63fbb6bc92613d09e7831325c519869b0c
Gabriel Pająk <pajak.gabriel@gmail.com>
2017-07-12 19:22:14 +0200

    Add menu animation. Add weapon menu controller.

---
+++ space-game/Assets/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    WeaponsMenu weaponsMenu;
+
+	// Use this for initialization
+	void Start () {
+        weaponsMenu = FindObjectOfType<WeaponsMenu>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            weaponsMenu.Open();
+        }
+        else
+        {
+            weaponsMenu.Close();
+        }
+	}
+}
---
+++ space-game/Assets/WeaponsMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsMenu : MonoBehaviour {
+
+    // Animation controller
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Open()
+    {
+        anim.SetBool("isOpen", true);
+    }
+
+    public void Close()
+    {
+        anim.SetBool("isOpen", false);
+    }
+}
